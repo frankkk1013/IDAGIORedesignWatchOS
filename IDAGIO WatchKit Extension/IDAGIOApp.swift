8616ca3827f044816ed384e7e6bb5803721897e7
frankkk1013 <59925828+frankkk1013@users.noreply.github.com>
2021-12-09 12:06:07 +0100

    Initial Commit

---
+++ IDAGIO WatchKit Extension/IDAGIOApp.swift
@@ -0,0 +1,21 @@
+//
+//  IDAGIOApp.swift
+//  IDAGIO WatchKit Extension
+//
+//  Created by Francesco Iaccarino on 09/12/21.
+//
+
+import SwiftUI
+
+@main
+struct IDAGIOApp: App {
+    @SceneBuilder var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+
+        WKNotificationScene(controller: NotificationController.self, category: "myCategory")
+    }
+}
---
+++ IDAGIO WatchKit Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  IDAGIO WatchKit Extension
+//
+//  Created by Francesco Iaccarino on 09/12/21.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
